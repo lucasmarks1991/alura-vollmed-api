fb521308479a2f4d000679eaf61069c1810e3bab
Lucas Marques Santos <lucasmarks1991@hotmail.com>
2024-08-10 08:25:08 -0300

    wip: adicionando projeto ao git

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,36 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+    private String logradouro;
+
+    private String bairro;
+
+    private String cep;
+
+    private String numero;
+
+    private String complemento;
+
+    private String cidade;
+
+    private String uf;
+
+    public Endereco(DadosEndereco dadosEndereco) {
+        this.logradouro = dadosEndereco.logradouro();
+        this.bairro = dadosEndereco.bairro();
+        this.cep = dadosEndereco.cep();
+        this.uf = dadosEndereco.uf();
+        this.cidade = dadosEndereco.cidade();
+        this.numero = dadosEndereco.numero();
+        this.complemento = dadosEndereco.complemento();
+    }
+}
