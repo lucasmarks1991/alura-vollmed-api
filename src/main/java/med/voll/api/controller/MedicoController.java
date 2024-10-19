d1c748c8a113bc8cf1084a16a146eb795b910705
Lucas Marques Santos <lucasmarks1991@hotmail.com>
2024-10-19 10:52:28 -0300

    feat: adicionado rota para listar médicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,15 +1,13 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/medicos")
@@ -21,5 +19,10 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dadosCadastroMedico) {
         this.medicoRepository.save(new Medico(dadosCadastroMedico));
+    }
+
+    @GetMapping("")
+    public Page<DadosListagemMedico> listar(@PageableDefault(sort = "nome") Pageable pageable) {
+        return this.medicoRepository.findAll(pageable).map(DadosListagemMedico::new);
     }
 }
