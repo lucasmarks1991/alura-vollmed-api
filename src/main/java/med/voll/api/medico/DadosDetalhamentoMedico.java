13e96d1b0c56c6366879affcfd8765b105bc7555
Lucas Marques Santos <lucasmarks1991@hotmail.com>
2024-11-01 18:28:30 +0000

    feat: adicionado campod e telefone no DTO de detalhamento de médico

--- src/main/java/med/voll/api/medico/DadosDetalhamentoMedico.java
+++ src/main/java/med/voll/api/medico/DadosDetalhamentoMedico.java
@@ -3,13 +3,20 @@ package med.voll.api.medico;
 import med.voll.api.endereco.DadosDetalhamentoEndereco;
 
 public record DadosDetalhamentoMedico(
-    Long id, String nome, String email, String crm, Especialidade especialidade, DadosDetalhamentoEndereco endereco
+    Long id,
+    String nome,
+    String email,
+    String telefone,
+    String crm,
+    Especialidade especialidade,
+    DadosDetalhamentoEndereco endereco
 ) {
     public DadosDetalhamentoMedico(Medico medico) {
         this(
             medico.getId(),
             medico.getNome(),
             medico.getEmail(),
+            medico.getTelefone(),
             medico.getCrm(),
             medico.getEspecialidade(),
             new DadosDetalhamentoEndereco(medico.getEndereco())
